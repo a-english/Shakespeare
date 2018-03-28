19028c4b3cb350d0265c4e0c889339e63434aeb2
Angela English <angelacaurienglish@csus.edu>
2018-03-27 18:01:18 -0700

    Added Correlator

---
+++ Correlator.java
@@ -0,0 +1,30 @@
+
+public class Correlator {
+
+	DataCounter dc1, dc2;
+	
+	public static void usage(){
+		System.out.print("This program compares the word frequency of two different txt files.\n"+
+		"Usage:	java Correlator [ -b | -a | -h ] [ -frequency | -num_unique ] <filename1> <filename2>\n"+
+		"-b\tUse an Unbalanced BST to implement the DataCounter\n"+
+		"-a\tUse an AVL Tree\n"+
+		"-h\tUse a Hashtable\n"+
+		"-frequency \tPrint all the word/frequency pairs, ordered by frequency, \n"+
+		"\t\tand then by the words in lexicographic order\n"+
+		"\n"+
+		"-num_unique \tPrint the number of unique words in the document. \n"+
+		"\t\tThis is the total number of distinct (different) words in the \n"+
+		"\t\tdocument. Words that appear more than onceare only counted \n"+
+		"\t\tas a single word for this statistic.\n");
+	}
+	
+	public Correlator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
